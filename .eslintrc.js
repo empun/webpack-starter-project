c33728ecdd702ab649bba6ad5bd4b993fd252ac1
potatocandie <maftukhatularifin13@gmail.com>
2022-04-08 18:15:23 +0700

    feat: Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        singleQuote: true,
+        semi: true,
+        trailingComma: 'es5',
+      },
+    ],
+    'no-console': 'off',
+  },
+};
---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+require('../scss/styles.scss');
+
+console.log('🚀 --- webpack starter project initialized --- 🚀');
